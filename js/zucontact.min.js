64fbc12df03b536a1fab608935b6429a7273da13
picasso <dmitry.rudakov@icloud.com>
2020-11-20 17:59:38 +0100

    added support for Google reCAPTCHA

--- js/zucontact.min.js
+++ js/zucontact.min.js
@@ -1,11 +1,20 @@
 (function($) {
 	// eslint-disable-next-line no-undef
 	var jsdata = (typeof zucontact_jsdata !== 'undefined' && zucontact_jsdata.data !== undefined) ? zucontact_jsdata.data : {};
+
 	var Cs_prefix = jsdata.prefix;
+	var Cs_processed = `${Cs_prefix}-processed`;
+	var Cs_errors = `${Cs_prefix}-general-errors`;
+
 	var Ds_container = `.${Cs_prefix}-container`;
 	var Ds_subheading = `.${Cs_prefix}-subheading`;
 	var Ds_status = `.${Cs_prefix}-status`;
+	var Ds_control= `.${Cs_prefix}-control`
 	var Ds_submit = `#${Cs_prefix}-submit`;
+
+	var Fn_verified = `${Cs_prefix}_verified`;
+	var Fn_expired = `${Cs_prefix}_expired`;
+	var Fn_network = `${Cs_prefix}_network`;
 	// set 0 for production
 	var debugDelay = 0;
 
@@ -19,46 +28,21 @@
 		$form = $container.find('form'),
 		$button = $form.find(Ds_submit);
 
+		var errorMessages = {
+			generic: $status.find('.message').data('errmsg'),
+			invalid: '',
+			expired: '',
+		}
+
 		if($container.length) {
 			// add classes which could be used in others elements
 			inlineStyle();
-			// var parent_offset = $container.parent().offset().top;
-			// var 	cplus_container_margin = Math.floor($container.offset().top - parent_offset),
-			// 		cplus_status_margin = Math.floor($status.offset().top - parent_offset),
-			// 		cplus_form_margin = Math.floor($form.offset().top - parent_offset);
-			//
-			// $(`<style type="text/css">
-			// 	cplus-container-margin{margin-top:'+cplus_container_margin+'px !important;}
-			// 	.cplus-subheading-margin{margin-top:'+cplus_status_margin+'px !important;}
-			// 	.cplus-form-margin{margin-top:'+cplus_form_margin+'px !important;} ' +
-			// 	</style>`).appendTo('body');
-			// if($subheading.length) $subheading.css({top: cplus_status_margin-cplus_container_margin});
+			// init reCAPTCHA callbacks if needed
+			initReCaptcha();
 		} else {
 			// something wrong but we do not want to break JS
 			return;
 		}
-
-		// NOTE: подключить captcha потом
-		// $form.find('#recaptcha_response_field').focus(function() {
-		//     $errele = $form.find(`div[for="${Cs_prefix}_recaptcha"]`);
-		//     $errele.html('');
-		// });
-
-		// NOTE: сделать валидацию на клиенте?
-		// $form.validate({
-		//
-		//     errorElement: 'span',
-		//
-		//     highlight: function(label, errorClass, validClass) {
-		//         $(label).closest('.form-group').removeClass('has-success').addClass('has-error');
-		//         $(label).closest('.control-group').removeClass('success').addClass('error'); // support for bootstrap 2
-		//
-		//     },
-		//     unhighlight: function(label, errorClass, validClass) {
-		//         $(label).closest('.form-group').removeClass('has-error').addClass('has-success');
-		//         $(label).closest('.control-group').removeClass('error').addClass('success'); // support for bootstrap 2
-		//     }
-		// });
 
 		function inlineStyle() {
 			var offset = $container.parent().offset().top;
@@ -76,46 +60,51 @@
 			if($subheading.length) $subheading.css({top: status_margin - container_margin});
 		}
 
-		function removePrevErrors(success) {
+		function removePrevErrors() {
 			$form.find(`.${Cs_prefix}-control.error`).removeClass('error');
-			$status.removeClass('sent was-error').addClass(success ? 'sent' : 'was-error');
 		}
 
-		function formatMessage(msg) {
+		function processMessage(errors, message) {
+
 			var msgLimit = 80;
-			if(msg.length > msgLimit) msg = `${msg.substring(0, msgLimit)}...`;
-			return `<span>${msg}</span>`;
+			var $message = $status.find('.message');
+			if(message === undefined) message = errorMessages.generic;
+
+			// maybe we have general AJAX, server or reCAPTCHA errors
+			var errMessage = errors.ajax || errors.recaptcha || null;
+			if(errMessage && errMessage.length > msgLimit) errMessage = `${errMessage.substring(0, msgLimit)}...`;
+			errMessage = errMessage ? `<span>${errMessage}</span>` : null;
+
+			if(errMessage) message = message.replace(/<b>[^<]+/g, `<b>${errMessage}`);
+			$message.html(message);
+		}
+
+		function switchHeading(isHeading, isError, isSuccess) {
+			$subheading[isHeading ? 'addClass': 'removeClass']('before_posting');
+			$container[isHeading ? 'removeClass': 'addClass'](Cs_processed);
+			$container[isError ? 'addClass': 'removeClass'](Cs_errors);
+			$status[isError ? 'addClass': 'removeClass']('was-error')[isSuccess ? 'addClass': 'removeClass']('sent');
 		}
 
 		function processPostData(data) {
-			var $message = $status.find('.message');
-			var msg = data.message === undefined ? $message.data('errmsg') : data.message;
+
 			var errors = data.errors || {};
-
-			// maybe we have general AJAX or server errors
-			if(errors.ajax) msg = msg.replace(/<b>[^<]+/g, '<b>' + formatMessage(errors.ajax));
-			$message.html(msg);
-
-			$container.addClass(`${Cs_prefix}-processed`).removeClass(`${Cs_prefix}-general-errors`);
+			processMessage(errors, data.message);
+			switchHeading(false, !data.is_valid, data.is_valid === true);
+			removePrevErrors();
 
 			if(data.is_valid === true) {
-				// remove previous errors and mark as 'sent'
-				removePrevErrors(true);
-
 				// if(isScrolledIntoView($div) === false) {
 				// 	$('html,body').animate({
 				// 		scrollTop: $($div.selector).offset().top
 				// 	}, 'slow');
 				// }
 			} else {
-				// remove previous errors and mark as 'was-error'
-				removePrevErrors(false);
-
 				$.each(errors, function(name, value) {
 					var $validated = $form.find(`span[for="${Cs_prefix}-${name}"]`);
 					if($validated.length) {
 						$validated.html(value);
-						$validated.closest(`.${Cs_prefix}-control`).addClass('error');
+						$validated.closest(Ds_control).addClass('error');
 					}
 				});
 			}
@@ -125,7 +114,6 @@
 
 		function ajaxLoading(initiated) {
 			if(initiated) {
-				$subheading.removeClass('before_posting');
 				$button.attr('disabled', 'disabled');
 			} else {
 				$button.removeAttr('disabled');
@@ -163,9 +151,65 @@
 
 		$form.submit(function(e) {
 			e.preventDefault();
+			if(needsReCaptcha()) return false;
 			ajaxLoading(true);
 			setTimeout(ajaxCall, debugDelay);
 		});
+
+		// Google reCAPTCHA V2 helpers ----------------------------------------]
+
+		function reVerified() {
+			switchHeading(true);
+			markReCaptcha(false);
+		}
+
+		function reExpired() {
+			processMessage({ recaptcha: errorMessages.expired });
+			switchHeading(false, true);
+			markReCaptcha(true);
+		}
+
+		function reNetwork() {
+			processMessage({ recaptcha: errorMessages.network });
+			switchHeading(false, true);
+			markReCaptcha(true);
+		}
+
+		function markReCaptcha(isError) {
+			$('.g-recaptcha')[isError ? 'addClass': 'removeClass']('error');
+		}
+
+		function initReCaptcha() {
+			var $recaptcha = $('.g-recaptcha');
+			if($recaptcha.length) {
+				window[Fn_verified] = reVerified;
+				window[Fn_expired] = reExpired;
+				window[Fn_network] = reNetwork;
+				$recaptcha.attr({
+					'data-callback': Fn_verified,
+					'data-expired-callback': Fn_expired,
+					'data-error-callback': Fn_network,
+				});
+				errorMessages.invalid = $recaptcha.data('invalid');
+				errorMessages.expired = $recaptcha.data('expired');
+				errorMessages.network = $recaptcha.data('network');
+			}
+		}
+
+		function needsReCaptcha() {
+			// maybe we should check recaptcha first
+			if(typeof grecaptcha !== 'undefined' && $('.g-recaptcha').length) {
+				// eslint-disable-next-line no-undef
+				var response = grecaptcha.getResponse();
+				if(response.length === 0) {
+					processMessage({ recaptcha: errorMessages.invalid });
+					switchHeading(false, true);
+					markReCaptcha(true);
+					return true;
+				}
+			}
+			return false;
+		}
 
 	}); // end of $(document).ready
 
@@ -181,3 +225,14 @@
 	// }
 
 })(jQuery);
+// var parent_offset = $container.parent().offset().top;
+// var 	cplus_container_margin = Math.floor($container.offset().top - parent_offset),
+// 		cplus_status_margin = Math.floor($status.offset().top - parent_offset),
+// 		cplus_form_margin = Math.floor($form.offset().top - parent_offset);
+//
+// $(`<style type="text/css">
+// 	cplus-container-margin{margin-top:'+cplus_container_margin+'px !important;}
+// 	.cplus-subheading-margin{margin-top:'+cplus_status_margin+'px !important;}
+// 	.cplus-form-margin{margin-top:'+cplus_form_margin+'px !important;} ' +
+// 	</style>`).appendTo('body');
+// if($subheading.length) $subheading.css({top: cplus_status_margin-cplus_container_margin});
