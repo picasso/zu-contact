fcabdd522b2af814e1853f3abb26da369aa83f0d
picasso <dmitry.rudakov@icloud.com>
2020-11-23 11:16:51 +0100

    adapted to 'shortcode_atts_with_cast' snippet

--- includes/zucontact-shortcode.php
+++ includes/zucontact-shortcode.php
@@ -14,31 +14,50 @@ trait zu_ContactShortcode {
 		return $this->shortcode(array_merge(['form' => 'booking'], $atts), $content);
 	}
 
+	private function cast($values, $types) {
+		if(is_array($types) && is_array($values)) {
+			foreach($types as $key => $type) {
+				if(array_key_exists($key, $values)) {
+					if($type === 'bool') $values[$key] = to_bool($values[$key]);
+				}
+			}
+		}
+		return $values;
+	}
+
 	public function shortcode($atts, $content = null) {
 
-		extract(shortcode_atts([
+		extract($this->snippets('shortcode_atts_with_cast', $atts, [
 			'form' 				=> 'contact',
 			'class' 			=> '',
 			'ajax' 				=> true,
 			'message' 			=> '',
-			'subheading'		=> '',
+			'subheading'		=> null,
+			'recaptcha'			=> -1,
 			'rows'				=> -1,
-		], $atts));
+		], [
+			'ajax' 				=> 'bool',
+			'recaptcha'			=> 'bool',
+		]));
 
 		// if $ajax is true enqueue both css and script (null), otherwise css only (true)
 		$this->enqueue_only($ajax ? null : true);
-		$this->maybe_enqueue_recaptcha();
+		// if 'recaptcha' is true but 'use_recaptcha' option is false - nothing will happen
+		// use 'recaptcha' set to false to disable 'recaptcha' when 'use_recaptcha' is true
+		$this->maybe_enqueue_recaptcha($recaptcha);
 
 		if(!in_array($form, $this->available_forms())) $form = 'contact';
 		if(!is_null($content)) $message = do_shortcode($content);
 
 	    $contact = new zu_ContactData;
-// _dbug($contact);
-	    if($contact->is_valid()) $contact->send_mail();
+_dbug($contact);
+		if($contact->has_recaptcha()) $this->check_recaptcha($contact);
+	    if($contact->is_valid()) $this->send_mail($contact);
 
 		$values = $contact->as_values();
 		$values['_was_sent'] = $contact->was_sent;
 		$values['_subheading'] = $subheading;
+		$values['_recaptcha'] = $recaptcha;
 		if($rows !== -1) $values['_rows'] = absint($rows);
 
 		return $this->sprint_form($form, $values, $contact->errors, $message, $class);
