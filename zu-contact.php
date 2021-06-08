a68aa48b598fc49e216b39ebb0e33cfe75c11aff
picasso <dmitry.rudakov@icloud.com>
2021-06-08 18:15:45 +0200

    changed loading order for Zukit

--- zu-contact.php
+++ zu-contact.php
@@ -15,6 +15,13 @@ Requires PHP: 7.2.0
 
 // Prohibit direct script loading
 defined('ABSPATH') || die('No direct script access allowed!');
+
+add_action('plugins_loaded', function() { 	// DEBUG ONLY
+
+// Always load Zukit even if we don't use it later ('wp_doing_ajax' or 'wp_doing_cron')
+// as other plugins or themes may want to use it
+require_once('zukit/load.php');
+
 // Exit early if a WordPress heartbeat comes
 if(wp_doing_ajax() && isset($_POST['action']) && ($_POST['action'] === 'heartbeat')) return;
 // Let's not load plugin during cron events
@@ -22,16 +29,11 @@ if(wp_doing_cron()) return;
 
 // Start! ---------------------------------------------------------------------]
 
-// add_action('plugins_loaded', function() { 	// DEBUG ONLY
-
-require_once('zukit/load.php');
-
 // compatibility check for Zukit
 if(Zukit::is_compatible(__FILE__)) {
 
 	require_once('includes/zucontact-plugin.php');
 	zucontact(__FILE__);
-
 }
 
-// });
+});
