d99ca385f5b266011f11450c0d47988237b73f41
picasso <dmitry.rudakov@me.com>
2025-02-05 10:13:00 +0100

    Squashed 'zukit/' changes from 20c23dd..f6883d2
    f6883d2 fix 'gitattributes' generation
    181e051 improve webpack configs
    0318234 add 'release' section
    34cec4b bump version 2.0.1
    4f60ffe small fix
    4ade6d2 update section about working project details
    66bf57d update 'script' dependencies based on generated by 'wp-scripts'
    b4e5c63 replace wordpress import from 'const' to 'import'
    3cdf3e2 improve import sorting
    764a5cb rebuild
    69437c2 implement 'dev' script with browser refresh
    7cacca9 improve 'build' scripts and config
    800d31c fix 'ignores' in eslint config
    99b6b66 change '@use' references in SASS
    df8f010 update 'gitattributes' ignore
    d4e0896 implement abstract protected function 'root' for 'zukit_Singleton' class
    22cca62 fix error with undefined '$options' in 'set_option'
    ad23bbe fix PHP formatting and some errors found by 'intelephense'
    ce21892 update formatting and 'deps' in 'script_defaults' function
    2754a6d refactor JS  with 'lodash-es' and prettier rules
    98a49e8 build all assets
    3ae7aed update outdated npm packages
    16f239e add 'wp-scropts' build and webpack configs for it
    8f3096d migrate SASS files to avoid deprecation warnings
    1357b68 add zukit workspace
    df3eaeb update 'translate-2-json' with new 'replace-in-file' version
    1a9ec44 replace 'classnames' package with the faster and smaller 'clsx' package
    b8f8b52 replace eslint config with v9
    0258904 add 'markdownlint' and fix .md files
    46d562b add prettier and its config
    6ed5731 add prettier config
    cd357b7 bump version 1.5.5
    8bb6114 removed 'zusnippets_Lang' trait (duplicated in the 'Zu Translate' plugin)
    e6f0571 delayed value creation for the second argument of 'logGroup' method
    061c034 renaming 'debug' JS data set to 'inhouse'
    4637768 refactoring init for '_debug' and '_panels' option groups
    9535057 keep '_debug' and '_panels' option groups even on reset
    2231b57 bump version 1.5.4
    cdff343 added 'unescapeHtml' helper to 'utils'
    b7a04cb added 'hover' to 'notice__dismiss' button
    3866701 fixed 'markdown' CSS in 'action' buttons
    b7b41e0 added 'zu_loge' function as last resort for complex cases
    d37c817 bump version 1.5.3
    624a740 fixed font for Safari
    8145342 improved inserting separators in the 'admin menu'
    39cf498 fix CSS of 'Settings' (Safari)
    b30eb39 small fix
    6151900 bump version 1.5.2
    40cf407 added outline on ':focus' for 'SelectItemControl' control
    e650f5c improved CSS on various screen widths
    b142f78 added 'dim' color modifier; refactoring 'logGroup' method
    fa47c27 changed opaque color for '^' modifier
    ab2d2cf delayed value creation - if the 'data' is a function, then we replace it with the value returned from the function
    5a9fd93 bump version 1.5.1
    e50135a made '$zukit_handle' public
    c7a0b58 fixed WP Core style, which suddenly lost in WP 5.9
    6f94b31 logo experiments
    
    git-subtree-dir: zukit
    git-subtree-split: f6883d2221672ebfde9e2e1efeb3d52f5d71a889

---
+++ wp-scripts.config.cjs
@@ -0,0 +1,38 @@
+/* eslint-disable no-undef */
+// See https://developer.wordpress.org/block-editor/reference-guides/packages/packages-scripts/#advanced-usage
+
+const defaultConfig = require('@wordpress/scripts/config/webpack.config')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const path = require('path')
+
+// remove default `RtlCssPlugin` plugin
+const defaultPlugins = defaultConfig.plugins.filter((p) => p.constructor?.name !== 'RtlCssPlugin')
+
+module.exports = {
+	...defaultConfig,
+	context: path.resolve(__dirname, 'src'),
+	entry: {
+		'zukit-blocks': ['./scripts/zukit-blocks.js', './sass/zukit-blocks.scss'],
+		zukit: ['./scripts/zukit.js', './sass/zukit.scss'],
+		// трюк, потому что `wp-scripts` все равно будет пытаться генерить JS скрипт
+		// мы потом всё удалим с помощью `CleanWebpackPlugin`
+		'zukit-colors': './sass/zukit-colors.scss',
+	},
+	output: {
+		filename: '[name].min.js',
+		path: path.resolve(__dirname, 'dist'),
+	},
+	plugins: [
+		...defaultPlugins,
+		new CleanWebpackPlugin({
+			protectWebpackAssets: false,
+			cleanOnceBeforeBuildPatterns: ['**/*', '!*.js'],
+			cleanAfterEveryBuildPatterns: ['*asset.php', '*-colors.min.js'],
+		}),
+	],
+	stats: {
+		children: false,
+		assets: false,
+		modules: false,
+	},
+}
